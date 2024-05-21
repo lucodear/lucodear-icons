0ff0aae40eb587689edfce2f7c4068e01b3fd3c4
Lucas Colombo <lucasncolombo@gmail.com>
2024-05-21 05:01:52 -0300

    feat: ✨ file icon release.toml

--- src/lucodear/icons/misc/files.ts
+++ src/lucodear/icons/misc/files.ts
@@ -94,6 +94,11 @@ export const files: LucodearFileIcon[] = lucodear('misc', [
     light: true,
   },
   {
+    name: 'release',
+    fileNames: ['release.toml', 'Release.toml'],
+    light: true,
+  },
+  {
     name: 'workflow',
     fileExtensions: [
       'workflow/yml',

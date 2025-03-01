5656ed3c25af5ab99820e7389c7b635d2c05b2ee
Lucas Colombo <lucasncolombo@gmail.com>
2025-03-01 15:18:07 -0300

    fix: cloned configured icons not working for lucodear specific icons

--- src/scripts/icons/generateClones.ts
+++ src/scripts/icons/generateClones.ts
@@ -1,12 +1,19 @@
 import { generateManifest } from '../../@lucodear/core/generators/manifest';
-import {} from '../../@lucodear/core/icons';
+import { applyLucodearOverrides } from '../../@lucodear/core/generators/override';
 import {
-  fileIcons,
-  folderIcons,
+  lucodearFileIcons,
+  lucodearFolderIcons,
+} from '../../@lucodear/core/icons';
+import {
+  type FileIcons,
+  type FolderTheme,
   generateConfiguredFileIconClones,
   generateConfiguredFolderIconClones,
   generateConfiguredLanguageIconClones,
   languageIcons,
+  merge,
+  fileIcons as originalFileIcons,
+  folderIcons as originalFolderIcons,
 } from '../../core';
 import { yellow } from '../helpers/painter';
 
@@ -15,7 +22,18 @@ import { yellow } from '../helpers/painter';
  */
 try {
   console.log('> Material Icon Theme:', yellow('Generating icon clones...'));
+
+  // #region 🍭 » lucode (changed with custom generateManifest and overrides)
   const manifest = generateManifest();
+  let [fileIcons, folderIcons] = applyLucodearOverrides(
+    originalFileIcons,
+    originalFolderIcons
+  );
+
+  fileIcons = merge(fileIcons, lucodearFileIcons as FileIcons);
+  folderIcons = [...folderIcons, ...[lucodearFolderIcons as FolderTheme]];
+  // #endregion
+
   generateConfiguredFileIconClones(fileIcons, manifest);
   generateConfiguredFolderIconClones(folderIcons, manifest);
   generateConfiguredLanguageIconClones(languageIcons, manifest);
